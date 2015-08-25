397cb8ba3fe6984933d6659b863f875140c61a5b
remus chang <remus.gc.chang@gmail.com>
2015-08-25 14:15:14 +0800

    final removed

--- mysite/trips/admin.py
+++
@@ -1,6 +0,0 @@
-# trips/admin.py
-
-from django.contrib import admin
-from trips.models import Post
-
-admin.site.register(Post)
--- mysite/trips/views.py
+++
@@ -1,7 +0,0 @@
-# trips/views.py
-from datetime import datetime
-#from django.http import HttpResponse
-from django.shortcuts import render
-
-def hello_world(request):
-    return render(request, 'hello_world.html', {'current_time':datetime.now()})
